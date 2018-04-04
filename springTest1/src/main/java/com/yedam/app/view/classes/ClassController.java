93ec978406604cc01f08f30710ac08b36d078c3f
User <User@YD02-04>
2018-04-04 17:52:08 +0900

    classcontroller 수정

--- springTest1/src/main/java/com/yedam/app/view/classes/ClassController.java
+++ springTest1/src/main/java/com/yedam/app/view/classes/ClassController.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.yedam.app.classes.ClassService;
 import com.yedam.app.classes.ClassVO;
@@ -24,43 +28,45 @@ public class ClassController {
 
 	@Autowired
 	ClassService classService;
-	
+
 	// 목록
 	@RequestMapping("getClassList")
 	public String getClassList(Model model, ClassVO vo) {
 		model.addAttribute("classList", classService.getClassList(vo));
 		return "class/getClassList";
 	}
-	
 
 	// 등록폼
 	@RequestMapping(value = "insertClass", method = RequestMethod.GET)
 	public String insertClassForm() {
 		return "class/insertClass";
-		
 	}
-	
+
 	// 등록처리
 	@RequestMapping(value = "insertClass", method = RequestMethod.POST)
-	public String insertClass(ClassVO vo) throws IOException {
+	public String insertClass(ClassVO vo, HttpServletRequest request, HttpServletResponse response) throws IOException {
+
 		// 첨부파일이 있는지 확인
-		MultipartFile file = vo.getTimeTable();
-		if (file != null && file.getSize() > 0) {
+		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+		MultipartFile multipartFile = multipartRequest.getFile("attach_file");
+
+		if (multipartFile != null && multipartFile.getSize() > 0) {
 			// 파일을 업로드 위치에 저장
-			file.transferTo(new File("D:/upload"));
-			vo.setTimeTable(file);
+
+			multipartFile.transferTo(new File("d:/upload", multipartFile.getOriginalFilename()));
+			vo.setTimeTable(multipartFile.getOriginalFilename());
 		}
 		classService.insertClass(vo);
 		return "redirect:/getClassList";
 	}
-	
+
 	// 수정폼
 	@RequestMapping("updateClassForm")
 	public String updateClassForm(Model model, ClassVO vo) {
 		model.addAttribute("class", classService.getClassList(vo));
 		return "class/updateClass";
 	}
-	
+
 	// 수정처리
 	@RequestMapping("updateClass")
 	public String updateClass(ClassVO vo, SessionStatus sessionStatus) {
@@ -68,19 +74,19 @@ public class ClassController {
 		sessionStatus.setComplete();
 		return "redirect:/getClassList";
 	}
-	
+
 	// 삭제
 	@RequestMapping("deleteClass")
 	public String deleteClass(ClassVO vo) {
 		classService.deleteClass(vo);
 		return "redirect:/getClassList";
 	}
-		
+
 	// 단건 삭제 처리
 	@RequestMapping("deleteClassList")
-	private String deleteClassList(@RequestParam ArrayList<String>cl_no) {
-		//ClassService.deleteClassList(cl_no);
+	private String deleteClassList(@RequestParam ArrayList<String> cl_no) {
+		classService.deleteClassList(cl_no);
 		return "redirect:/getClassList";
-		
+
 	}
 }
