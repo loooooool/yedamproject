778e23e1bfd12085f8b4e280d9355767a2038149
User <User@YD02-07>
2018-04-20 15:37:16 +0900

    정상완료

--- springTest1/src/main/java/com/yedam/app/member/view/MemberController.java
+++ springTest1/src/main/java/com/yedam/app/member/view/MemberController.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
@@ -55,9 +54,7 @@ public class MemberController {
 	@Autowired
 	CodeDAO dao;
 	
-	@Autowired
-	BCryptPasswordEncoder passwordEncoder;
-
+	
 
 	@RequestMapping("/getMemberList")
 	public String getBoardList(Model model, MemberVO vo, Paging paging) {
