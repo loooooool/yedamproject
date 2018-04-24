d66e76dcd79a0f68c0274420d48171f5904d50e3
User <User@YD02-07>
2018-04-24 11:52:24 +0900

    출석부 등록 기능

--- springTest1/src/main/java/com/yedam/app/view/timetable/TimeTableController.java
+++ springTest1/src/main/java/com/yedam/app/view/timetable/TimeTableController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.yedam.app.classes.ClassService;
 import com.yedam.app.classinfo.ClassInfoService;
@@ -64,24 +65,53 @@ public class TimeTableController {
 	//재용이가 짠거
 	//insert 폼
 	@RequestMapping("/insertTimeTableForm")
-	public String insertViewTimeTableForm() {
+	public String insertViewTimeTableForm(Model model) {
+		//List<ClassVO> list = ;
+		model.addAttribute("classList", classService.getClassList2(null));
 		return "timetable/timetableinsertview";
 	}
 	//insert 처리
 	@RequestMapping("/insertTimeTableView")
-	public String insertViewTimeTable() {
-		List<Map<String,Object>> list = sampleService.getExcelTimeTable(null);
-		Map<String,Object> input;
+	public String insertViewTimeTable(Model model, @RequestParam int sub_no) {
+		/*List<Map<String,Object>> list = sampleService.getExcelTimeTable(null);
+		Map<String,Object> input=null;
+		Map<String,Object> value=null;
 		
 		for(int i=0;i<list.size();i++) {
+			System.out.println("일자1 : "+list.get(i).get("day"));
+			System.out.println("시간 1: "+list.get(i).get("time").toString().replaceAll(":", ""));
+			System.out.println("과목 1: "+list.get(i).get("subject"));
+			System.out.println("코드값 : "+sub_no);
+			input = new HashMap<String,Object>();
+			value = new HashMap<String,Object>();
+			input.put("subject",list.get(i).get("subject"));
+			input.put("cl_no",sub_no );
 			
-		}
-		if(sampleService.checkTimeTable().isEmpty()) {
-			System.out.println("데이터가 없습니다.");
-			
-		}
+			String code_no= (String)sampleService.convertCode(list.get(i).get("time").toString().replaceAll(":", "")).get("code_no");
+			BigDecimal su_no = (BigDecimal)(sampleService.convertSubject(input).get("su_no"));
+			System.out.println(sampleService.getRowNum());
+			System.out.println("코드 : "+code_no);
+			System.out.println("코드2 : "+su_no);
+			System.out.println("max : "+(((BigDecimal)(sampleService.getRowNum().get("rn"))).intValue()+1));
+			if(sampleService.checkTimeTable().isEmpty()) {
+				value.put("t_id", "s1");
+				value.put("s_date", list.get(i).get("day"));
+				value.put("classtime_cd", code_no);
+				value.put("subject", su_no);
+				value.put("temp", 1);
+				sampleService.insertTimeTableAtt(value);
+			}else {
+				value.put("t_id", "s"+(((BigDecimal)(sampleService.getRowNum().get("rn"))).intValue()+1));
+				value.put("s_date", list.get(i).get("day"));
+				value.put("classtime_cd", code_no);
+				value.put("subject", su_no);
+				value.put("temp", (((BigDecimal)(sampleService.getRowNum().get("rn"))).intValue()+1));
+				sampleService.insertTimeTableAtt(value);
+			}
+		}*/
 		
-		
+		sampleService.insertViewTimeTable(model,sub_no);
+		model.addAttribute("classList", classService.getClassList2(null));
 		
 		return "timetable/timetableinsertview";
 	}
--- springTest1/src/main/webapp/WEB-INF/views/timetable/timetableinsertview.jsp
+++ springTest1/src/main/webapp/WEB-INF/views/timetable/timetableinsertview.jsp
@@ -1,10 +1,17 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+   <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head><title>Insert title here</title></head>
 <body>
-<form action="/insertTimeTableView">
+<form action="./insertTimeTableView">
+<select name="sub_no" id="sub_no">
+	<c:forEach items="${classList}" var="list">
+		<option value="${list.cl_no}">${list.class_name}</option>
+	</c:forEach>
+
+</select>
 <input type="submit" value="시간표등록"/>
 </form>
 </body>
